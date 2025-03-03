ffbd30f0f316db8b901e4e3afdf810166fb3cde5
RonOlen <altkontozq@gmail.com>
2025-03-03 15:18:35 +0200

    Add files via upload

---
+++ Program.cs uhuu.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int uhuu = 3;
+
+for (int i = 0; i < uhuu; i++)
+
+    Console.WriteLine("Tere uhuu");
