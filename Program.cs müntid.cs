d373d02b683e15a29cef6a214ddbfd4e12f329a4
RonOlen <altkontozq@gmail.com>
2025-03-03 15:06:36 +0200

    Add files via upload

---
+++ Program.cs müntid.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// vaja arvutada, mitu kahe eurost läheb ümber maa
+// maa ümbermõõt tuleb vaja arvutada raadiuse abil
+Console.WriteLine("Euro münti ruumala on 5cm")
+
+ double coinDiameter =  25.75; // mm
+double earthRadius = 6378000000; // mm
+double aroundEarth = 2 * Math.PI * earthRadius
+
+   
+    Console.WriteLine("Maailmas Ümbermõõt:" + aroundEarth);
+//nyyd alles muntide arv välja arvutada
+double howManyCoins = aroundEarth / coinDiameter
+
+Console.WriteLine("Maailmase mahub" + howManyCoins);
