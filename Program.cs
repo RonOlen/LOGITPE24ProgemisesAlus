6816f58ad62b6b0ca55e24ae2b341173cd83a332
RonOlen <altkontozq@gmail.com>
2025-03-03 13:49:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-cons,ole-template for more information
+Console.WriteLine("Hello World!");
+//peab saama sisestada nime
+//if ja else kontrollib, kas on nimi vi on tühi
+//kui leiate kuskilt teksti värviliseks teha, siis kasutage
+Console.WriteLine("Mis teie nimi on!");
+string name = Console.ReadLine();
+
+if (name != "")
+
+    Console.WriteLine("Õige nimi: " + name);
+
+else
+
+    Console.WriteLine("Vale nimi");
+Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("ERROR");
