56f4d616910f2aa2a43d4c9134fccfaad2d6d57d
RonOlen <altkontozq@gmail.com>
2025-03-03 14:02:31 +0200

    Add files via upload

---
+++ Program.cs mata.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Kui palju on 3 + 8 / (4 - 2) * 4 = ?");
+
+//miks arvuti seliselt arvutab?
+double calculation1 = 3 + 8 / (4 - 2) * 4;
+double calculation2 = (3 + 8) / (4 - 2) * 4;
+double calculation3 = (3 + 8) / (4 - 2) * 4;
+double calculation4 = (3.0 + 8.0) / (4.0 - 2.0) * (4.0);
+
+Console.WriteLine("Vastus:" + calculation1);
+Console.WriteLine("Vastus:" + calculation2);
+Console.WriteLine("Vastus:" + calculation3);
+Console.WriteLine("Vastus:" + calculation4);
+ 
