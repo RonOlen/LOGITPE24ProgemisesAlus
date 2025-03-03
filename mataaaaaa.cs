ca425d74f20cda8c28105fefe4a871caf4ea5549
RonOlen <altkontozq@gmail.com>
2025-03-03 15:51:41 +0200

    Add files via upload

---
+++ mataaaaaa.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// kirjuta programm ,mis väljastab kill-koll x5 a siis killadi-koll x1 ja kill-koll x3 ja killadi-koll*/
+//for loop for loopi sees
+
+int arv = Int16.Parse(Console.ReadLine());
+
+for (int i = 0; i < arv; i++)
+
+Console.WriteLine("See rida on nr {0} ", i);
+string a = "kill-koll";
+string b = "killadi-koll";
+
+for (int j = 1; j <= 2; j++)
+
+    Console.WriteLine(a);
+
+for (int k = 0; k <= length; k++)
+
+    Console.WriteLine(b);
+
+
+
+
+
+
+
+
+
+
+
