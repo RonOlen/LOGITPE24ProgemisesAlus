43ad675e47e584961d089dee4ada6134c27c1e2b
RonOlen <altkontozq@gmail.com>
2025-03-04 11:11:24 +0200

    Add files via upload

---
+++ Paaris v paaritu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel.Design;
+
+namespace IFsAndOddsnumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // konsool kysib numbrit ja siis muudetakse
+            // see int andmetüübiks
+
+            //teha if ja else-ga kontroll
+            // kus saame teada et kas nr on paris voi paaritu
+            // kasuta operaatoreid
+
+          
+
+            Console.WriteLine("Sisesta number");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            Console.WriteLine("Paaris");
+            
+            else
+
+                Console.WriteLine("paaritu");
+
+
+
+
+            
+
+
+
+        }
+    }
+}
