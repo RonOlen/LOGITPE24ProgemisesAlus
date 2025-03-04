a45eb5f8f658d4bf42dbeef8d30e4eff0c2d5abb
RonOlen <altkontozq@gmail.com>
2025-03-04 10:32:53 +0200

    Add files via upload

---
+++ Program.cs skibidi.cs
@@ -0,0 +1,31 @@
+﻿namespace IFsAndElses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+            // if ja else rakendus tuleb yeha
+            //kontrollib sinu vaunust
+            //1. Kui oled alla 18. siis konsool annab vastuse, et olen alaealine
+            
+
+            //2 Kui oled 19 kuni 65 aastat vana siis
+            // konsool vastab, et oled täisealine
+
+            //3Kui oled üle 65.a vana siis oled pensionäär
+        {
+            Console.WriteLine("Vana sa oled");
+                int age = int.Parse(Console.ReadLine());
+
+            if (age < 18)
+                Console.WriteLine("Oled alaealine");
+
+            else if (age >= 18 && age <= 64)
+
+                Console.WriteLine("Oled täiskasvanud");
+            else if (age >= 65)
+
+                Console.WriteLine("Oled pensionäär");
+        }
+    }
+}
