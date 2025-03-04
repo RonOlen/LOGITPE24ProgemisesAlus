9cb5639237f75a713de80ad69399dba0259b9fcc
RonOlen <altkontozq@gmail.com>
2025-03-04 11:24:57 +0200

    Add files via upload

---
+++ Jalgrattas.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //kutsuda esile meetod Jalgratas
+
+            Jalgratas();
+
+
+               
+        }
+
+        static void Jalgratas()
+        {
+            Console.WriteLine("nüüd oled jalgrata meetodis ");
+        }
+    }   
+}
